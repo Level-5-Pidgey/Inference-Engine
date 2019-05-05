d809205c5a981091a0ccf618c4931fa96e1ce837
Carl Bird <carl_a_bird@hotmail.com>
2019-05-06 09:51:03 +1000

    Add project files.

---
+++ Inference Engine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inference_Engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
